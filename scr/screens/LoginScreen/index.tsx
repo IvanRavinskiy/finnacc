8d6c6b758d39eced94f7b508ccb8d0ce26fae9e5
iwann <ivan.ravinskiy@gmail.com>
2022-05-23 15:57:51 +0300

    add login button

--- scr/screens/LoginScreen/index.tsx
+++ scr/screens/LoginScreen/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
+import {loginStyles} from './style';
 
 export const Login = () => {
+  const onGoogleLoginPress = () => {
+    console.log('onGoogleLoginPress click');
+  };
+
   return (
-    <View>
+    <View style={loginStyles.mainContainer}>
       <Text>login</Text>
+      <View style={loginStyles.btn}>
+        <Button title="Google Sign-In" onPress={onGoogleLoginPress} />
+      </View>
     </View>
   );
 };
---
+++ scr/screens/LoginScreen/style.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const loginStyles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    height: 50,
+    width: 200,
+  },
+});
