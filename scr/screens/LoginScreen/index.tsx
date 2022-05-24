deb7c73b35663d96aef4601d6b8bdee5ff5114a9
iwann <ivan.ravinskiy@gmail.com>
2022-05-24 18:15:39 +0300

    refactor login screen

--- scr/screens/LoginScreen/index.tsx
+++ scr/screens/LoginScreen/index.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, View} from 'react-native';
 import {loginStyles} from './style';
 import {useDispatch} from 'react-redux';
 import {isGoogleLogin} from '../../store/reducers/loginSlice';
-import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
 
 export const Login = () => {
   const dispatcch = useDispatch();
   const onGoogleLoginPress = () => {
-    console.log('onGoogleLoginPress click');
     dispatcch(isGoogleLogin());
   };
 
   return (
     <View style={loginStyles.mainContainer}>
-      <Text>login</Text>
       <View style={loginStyles.btn}>
         <Button title="Google Sign-In" onPress={onGoogleLoginPress} />
       </View>
-      <GoogleSigninButton
-        style={loginStyles.btn}
-        size={GoogleSigninButton.Size.Standard}
-        color={GoogleSigninButton.Color.Dark}
-        onPress={onGoogleLoginPress}
-      />
     </View>
   );
 };
