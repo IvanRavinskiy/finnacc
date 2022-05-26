b0754f04ab4a7065c3a31f485c09805d55c43f0c
iwann <ivan.ravinskiy@gmail.com>
2022-05-26 11:37:30 +0300

    add realtime db

--- scr/screens/HomeScreen/index.tsx
+++ scr/screens/HomeScreen/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ScrollView, View} from 'react-native';
 import {SvgPlus} from '../../assets/svg/SvgPlus';
 import {homeStyles} from './style';
@@ -9,6 +9,7 @@ import {useNavigation} from '@react-navigation/native';
 import {Screen} from '../../enums/Screen';
 import type {HomeNavigationProp} from './type';
 import {HeaderContainer} from '../../components/HeaderContainer';
+import {firebase} from '@react-native-firebase/database';
 
 export const Home = () => {
   const navigation = useNavigation<HomeNavigationProp>();
@@ -24,6 +25,19 @@ export const Home = () => {
       id: '',
     });
   };
+
+  useEffect(() => {
+    firebase
+      .app()
+      .database(
+        'https://new-finnacc-default-rtdb.europe-west1.firebasedatabase.app',
+      )
+      .ref('/expenses')
+      .once('value')
+      .then(snapshot => {
+        console.log(snapshot.val());
+      });
+  });
 
   return (
     <HeaderContainer>
