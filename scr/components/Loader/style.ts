4c0b08feb5aac9bca704d4dfb9e3e4b926c41fea
iwann <ivan.ravinskiy@gmail.com>
2022-05-31 15:36:19 +0300

    fix loader style

--- scr/components/Loader/style.ts
+++ scr/components/Loader/style.ts
@@ -2,7 +2,9 @@ import {StyleSheet} from 'react-native';
 
 export const loaderStyles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     margin: 10,
